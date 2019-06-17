44f990e43271fa5901b0334ca6374fc179a0940f
Ken Hawkins <khawkins98@gmail.com>
2019-06-17 12:46:51 +0200

    Template tweaks

--- src/components/vf-bioimaging-banner/vf-bioimaging-banner.js
+++ src/components/vf-bioimaging-banner/vf-bioimaging-banner.js
@@ -64,7 +64,6 @@ function vfBioimagingBanner(scope) {
     }
 
   });
-
 }
 
 /**
@@ -90,23 +89,19 @@ function vfBioimagingBannerInsert(banner,bannerId,scope) {
       <img class="vf-bioimaging-banner__logo" src="/vf-eleventy/assets/vf-bioimaging-banner/assets/eubi-logo_icon.png" />
     </a>
     <div class="vf-text | vf-bioimaging-banner__message">
-      <div class="vf-text--heading-l">This is a <a class="vf-link" href="JavaScript:Void(0);">Euro-BioImaging</a> connected resource</div>
+      <div class="vf-text--heading-xl | vf-bioimaging-banner__message--headline">This is a <a class="vf-link" href="JavaScript:Void(0);">Euro-BioImaging</a> connected resource</div>
       <div class="vf-lede"><a href="#" class="vf-link">Learn more</a></div>
     </div>
     <div class="vf-bioimaging-banner__credit | vf-text">
-      <img class="vf-bioimaging-banner__logo" src="/vf-eleventy/assets/vf-bioimaging-banner/assets/BioImage_Archive.png" />
-      <div class="vf-text--body-s">Powered by the <a href="https://www.ebi.ac.uk/bioimage-archive" class="vf-link">BioImage Archive</a></div>
+      <img class="vf-bioimaging-banner__credit--logo" src="/vf-eleventy/assets/vf-bioimaging-banner/assets/BioImage_Archive.png" />
+      <span class="vf-text | vf-bioimaging-banner__credit--text">Powered by the <a href="https://www.ebi.ac.uk/bioimage-archive" class="vf-link">BioImage Archive</a></span>
     </div>
-
   `;
-
 
   generatedBannerHtml += '</div>';
 
   // set the html of the banner
   targetBanner.innerHTML = generatedBannerHtml;
-
 }
-
 
 export { vfBioimagingBanner };
