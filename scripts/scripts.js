c4c4c72b6abf5693334aa4c87ed504b4660d5365
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-17 11:36:09 +0000

    Deploy ebiwd/bioimaging-banner-demonstration to github.com/ebiwd/bioimaging-banner-demonstration.git:gh-pages

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -350,32 +350,8 @@ function vfBioimagingBannerInsert(banner,bannerId,scope) {
 /*
  *
  * scripts.css
- * The Visual Framework kitchen sink of JavaScript.
- * Import this as a quick way to get *everything*,
+ * Just the bits of the VF needed for the BioImaging banner.
  *
  */
 vfBanner();
 vfBioimagingBanner();
-
-// import { vfMastheadSetStyle } from 'vf-masthead/vf-masthead';
-// vfMastheadSetStyle();
-//
-// import { vfTabs } from 'vf-tabs/vf-tabs';
-// vfTabs();
-//
-// import 'vf-form/vf-form__core/assets/float-labels.js';
-//
-// // All EMBL JS
-// import { emblConditionalEdit } from 'embl-conditional-edit/embl-conditional-edit';
-// emblConditionalEdit();
-//
-// import { emblContentHubLoaderHtmlImports } from 'embl-content-hub-loader/embl-content-hub-loader__html-imports';
-// import { emblContentHubFetch } from 'embl-content-hub-loader/embl-content-hub-loader__fetch';
-// import { emblContentHub } from 'embl-content-hub-loader/embl-content-hub-loader';
-// emblContentHub();
-//
-// import { emblBreadcrumbs } from 'embl-breadcrumbs-lookup/embl-breadcrumbs-lookup';
-// emblBreadcrumbs();
-//
-// import { emblContentMetaProperties_Read } from 'embl-content-meta-properties/embl-content-meta-properties';
-// // No default invokation
