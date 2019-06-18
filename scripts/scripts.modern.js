40fe52ff6bd3330fdf9eebefa5dcf4d2297c3780
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-18 13:23:30 +0000

    Deploy ebiwd/bioimaging-banner-demonstration to github.com/ebiwd/bioimaging-banner-demonstration.git:gh-pages

---
+++ scripts/scripts.modern.js
@@ -0,0 +1,12 @@
+/*
+ *
+ * scripts.css
+ * Just the bits of the VF needed for the BioImaging banner.
+ *
+ */
+
+import { vfBanner } from 'vf-banner/vf-banner';
+vfBanner();
+
+import { vfBioimagingBanner } from 'vf-bioimaging-banner/vf-bioimaging-banner';
+vfBioimagingBanner();
